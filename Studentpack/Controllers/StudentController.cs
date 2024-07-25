899106cfa3adcb25b17edce86e46c75a28305f4c
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-26 05:05:09 +0530

    Fully Crud

--- Studentpack/Controllers/StudentController.cs
+++ Studentpack/Controllers/StudentController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Studentpack.Data;
 using Studentpack.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Studentpack.Controllers
 {
@@ -10,12 +13,117 @@ namespace Studentpack.Controllers
 
         public StudentController(ApplicationDbContext Db)
         {
-            _Db=Db;
+            _Db = Db;
         }
+
         public IActionResult Index()
         {
-            List<Student> obj=_Db.students.ToList();
-            return View(obj);
+            var students = _Db.students.ToList();
+            return View(students);
+        }
+
+        // GET: Student/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var student = await _Db.students
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StudentId,FirstName,LastName,DateOfBirth,Email,ClassId")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _Db.Add(student);
+                await _Db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await _Db.students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StudentId,FirstName,LastName,DateOfBirth,Email,ClassId")] Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _Db.Update(student);
+                    await _Db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Student/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _Db.students
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _Db.students.FindAsync(id);
+            _Db.students.Remove(student);
+            await _Db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _Db.students.Any(e => e.StudentId == id);
         }
     }
 }
